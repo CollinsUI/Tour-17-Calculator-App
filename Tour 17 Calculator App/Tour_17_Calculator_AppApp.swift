e3a63621463b81f364530416bddb863bce9d9a1b
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2022-11-10 06:20:17 +0300

    Initial Commit

---
+++ Tour 17 Calculator App/Tour_17_Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tour_17_Calculator_AppApp.swift
+//  Tour 17 Calculator App
+//
+//  Created by Collins Muthomi on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tour_17_Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
